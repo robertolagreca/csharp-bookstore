2132ecde3cb7dab54c3628a2ac3b14fb1bd0db74
robertolagreca <robertolagreca92@gmail.com>
2022-11-22 13:34:52 +0100

    Inizializzazione variabili ed assegnazione.

--- BookStoreCSharp/Program.cs
+++ BookStoreCSharp/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string title, author, codeISBN, availabilityKindle, bookCover;
+int pages, numbersReview;
+float bookWeightGrams, bookWidthCm, bookHeightCm, bookDepthCm, userMeanScore;
+
+title = "Il deserto dei tartari";
+author = "Dino Buzzati";
+codeISBN = "978-8804735106";
+
+pages = 300;
+bookWeightGrams = 250f;
+bookWidthCm = 14f;
+bookHeightCm = 21.6f;
+bookDepthCm = 2.3f;
+
+userMeanScore = 4.5f;
+numbersReview = 953;
+
+availabilityKindle = "Disponibile in versione KINDLE";
+bookCover = "Copertina flessibile disponibile";
+
+
